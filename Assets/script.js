92fa12047acd9ef388c50e7ea824076a191163ca
maddy <maddykimborowicz@gmail.com>
2021-12-17 19:51:36 -0800

    added local storage aspect for previous searches and readme

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,10 @@ var input = document.querySelector("#city-input");
 var searchBtn = document.querySelector("#search-form");
 var currentContainer = document.querySelector("#current");
 var forecastContainer = document.querySelector("#forecast");
+var searchHistoryContainer = document.querySelector('#previous');
+var searchHistory = [];
 
+// function to take the user input(city entered) and create variable for it
 function handleFormSubmit(event) {
   event.preventDefault();
   var city = input.value.trim();
@@ -12,6 +15,7 @@ function handleFormSubmit(event) {
   input.value = "";
 }
 
+// function to put city into the api and saves search to local storage
 function getCity(city) {
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=276a0385c550d64519820d182928a8c4`;
 
@@ -21,11 +25,12 @@ function getCity(city) {
     })
     .then(function (data) {
       oneCall(data);
+      saveSearchToLocaStorage(city);
     });
 }
 
+// takes latitude and longitude from previous search and puts it into the onecall api to grab the information needed
 function oneCall(weatherData) {
-  // console.log(weatherData);
 
   var cityName = weatherData.name;
   var lat = weatherData.coord.lat;
@@ -43,6 +48,7 @@ function oneCall(weatherData) {
     });
 }
 
+// creates variables for the current weather forecast
 function currentWeather(current, city) {
   console.log(current);
   var icon = current.weather[0].icon;
@@ -64,9 +70,11 @@ function currentWeather(current, city) {
   var uvEl = document.createElement("li");
   var iconEl = document.createElement("img");
 
+  // add classes to the elements we just created
   card.setAttribute("class", "card");
   cardHeader.setAttribute("class", "card-header");
 
+  // add text content to the elements we created
   cardTitle.textContent = "Current Weather";
   cardHeader.textContent = city + " - " + moment().format("MMMM Do YYYY");
   tempEl.textContent = "Temp: " + temp + "°F";
@@ -74,15 +82,13 @@ function currentWeather(current, city) {
   humidityEl.textContent = "Humidity: " + humidity + "%";
   uvEl.textContent = "UV Index: " + uv;
 
+  // add image source for the weather icons
   iconEl.src = "http://openweathermap.org/img/w/" + icon + ".png";
 
   listGroup.append(tempEl, windEl, humidityEl, uvEl);
   card.append(cardHeader, listGroup);
   cardHeader.append(iconEl);
-
   currentContainer.append(cardTitle, card);
-
-  console.log(icon);
 }
 
 function forecastWeather(daily, city) {
@@ -92,14 +98,16 @@ function forecastWeather(daily, city) {
   cardTitle.textContent = "5 Day Forecast";
   forecastContainer.append(cardTitle);
 
-  for (let i = 0; i < 5; i++) {
+  // for loop to create cards and data for each day of the 5 day forecast
+  for (let i = 1; i <= 5; i++) {
     var icon = daily[i].weather[0].icon;
     var temp = daily[i].temp.day;
     var wind = daily[i].wind_speed;
     var humidity = daily[i].humidity;
-    var date = moment().add(1, "days").format("MM/D/YY");
-    console.log(wind);
+    var date = moment().add(i, "days").format("MM/D/YY");
 
+    // create elements we need to display on forecast
+    var cardContainer = document.createElement("div");
     var card = document.createElement("div");
     var cardHeader = document.createElement("div");
     var listGroup = document.createElement("ul");
@@ -108,22 +116,74 @@ function forecastWeather(daily, city) {
     var humidityEl = document.createElement("li");
     var iconEl = document.createElement("img");
 
-    card.setAttribute("class", "card");
-    card.setAttribute("class", "forecast");
-    card.setAttribute("style", "max-width: 200px");
+    // add classes/styling to cards for forecast
+    cardContainer.setAttribute("class", "col-md");
+    card.setAttribute("class", " card ");
     cardHeader.setAttribute("class", "card-header");
 
+    // add text content to each card using the elements we created
     cardHeader.textContent = date;
     tempEl.textContent = "Temp: " + temp + "°F";
     windEl.textContent = "Wind: " + wind + " MPH";
     humidityEl.textContent = "Humidity: " + humidity + "%";
 
+    // add image source for the weather icons
     iconEl.src = "http://openweathermap.org/img/w/" + icon + ".png";
-
+   
+    // appended the elements so they appear on the site
     cardHeader.append(iconEl);
     listGroup.append(tempEl, windEl, humidityEl);
     card.append(cardHeader, listGroup);
-    forecastContainer.append(card);
+    cardContainer.append(card);
+    forecastContainer.append(cardContainer);
   }
 }
+
+// function to save each city you search to local storage
+function saveSearchToLocaStorage(city){
+  //to make sure city is not duplicated in local storage
+if(searchHistory.indexOf(city)!== -1){
+  return;
+}
+
+searchHistory.push(city);
+localStorage.setItem('search', JSON.stringify(searchHistory));
+createHistoryBtns()
+
+}
+
+
+function createHistoryBtns(){
+  searchHistoryContainer.innerHTML = ""
+
+  // loop to create button for each search item that goes into local storage
+  for (var i = 0; i < searchHistory.length; i++) {
+    
+    var btn =  document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('class', 'search-button mt-2')
+    btn.setAttribute('city-name', searchHistory[i])
+
+    btn.textContent = searchHistory[i]
+    searchHistoryContainer.append(btn)
+  }
+}
+
+// makes buttons appear on the side of the webpage
+function loadBtns(){
+  if(localStorage.getItem('search')){
+    searchHistory = JSON.parse(localStorage.getItem('search'))
+  }
+
+  createHistoryBtns()
+}
+
+// makes buttons appear on the side of the webpage right when the webpage is opened
+function historyBtnClick(event){
+  var historyClick = event.target.getAttribute('city-name');
+  getCity(historyClick)
+}
+loadBtns()
+
+searchHistoryContainer.addEventListener('click', historyBtnClick)
 searchBtn.addEventListener("submit", handleFormSubmit);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # weather-dashboard
+
+This weather dashboard allows a user to simply type in any city and the webage will show the current conditions including the temperature, wind, humidity, uv index, and an icon showing the current conditions.
+
+Below the user can also find a 5 day forecast with the same information. On the left side-bar, the users previous searches will show up so that they can easily go back and view a city they commonly search for.
+
+**Link to Webpage**
+
+https://mkimborowicz.github.io/weather-dashboard/
+
+
+**Screenshot of Webpage**
+
+![](./Assets/screenshot.png)
